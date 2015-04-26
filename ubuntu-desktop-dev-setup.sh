a019a5ae2b0c3bda788bf435c3f681165a7f5912
ifool <lifuzho@126.com>
2015-04-26 18:35:48 +0800

    Update ubuntu-desktop-dev-setup.sh

--- ubuntu-desktop-dev-setup.sh
+++ ubuntu-desktop-dev-setup.sh
@@ -1,4 +1,5 @@
-sudo apt-get install openssh-server mysql-server vim tmux
+sudo apt-get update
+sudo apt-get install curl openssh-server mysql-server libmysqlclient-dev vim tmux nginx 
 
 curl https://raw.githubusercontent.com/ifool/dev_conf/master/.tmux-ubuntu.conf > ~/.tmux.conf
 
@@ -7,3 +8,9 @@ curl https://raw.githubusercontent.com/ifool/dev_conf/master/.tmux-ubuntu.conf >
 "oh my zsh
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 
+" Ubuntu 12.04 上使用 Nginx Passenger 部署 Ruby on Rails: https://github.com/ruby-china/ruby-china/wiki/Ubuntu-12.04-%E4%B8%8A%E4%BD%BF%E7%94%A8-Nginx-Passenger-%E9%83%A8%E7%BD%B2-Ruby-on-Rails
+
+" Development Softwares: 
+"   SublimeText 3: http://www.sublimetext.com/3
+"   Google Chrome: https://www.google.com/chrome/browser/desktop/
+"   RubyMine: https://www.jetbrains.com/ruby/
