e98724df68b18cb5aac1dbabfd9f2e11aa13648d
ifool <lifuzho@126.com>
2015-04-26 20:23:22 +0800

    Update ubuntu-terminal.sh

--- ubuntu-terminal.sh
+++ ubuntu-terminal.sh
@@ -1,1 +1,3 @@
 gnome-terminal --maximize --working-directory="work/vcooline" --tab --working-directory="work/svn" --title=SVN --tab --title=Server --tab --title=Console --tab --title=Vcooline --tab --title=Vcooline --tab --working-directory="work/vcooline_cms" --title="CMS" --tab  --working-directory="." --title="Home" --tab  --working-directory="." --title="Home"; exit
+" In order to make gnome-terminal's title work, comment this line in ~/.zshrc
+" DISABLE_AUTO_TITLE="true"
