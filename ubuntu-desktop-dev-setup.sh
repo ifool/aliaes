59c7179d6d8928a6aaf816c54d422bc72f248a60
ifool <lifuzho@126.com>
2015-05-07 23:19:33 +0800

    Update ubuntu-desktop-dev-setup.sh

--- ubuntu-desktop-dev-setup.sh
+++ ubuntu-desktop-dev-setup.sh
@@ -1,7 +1,7 @@
 sudo apt-get update
 sudo apt-get install curl openssh-server mysql-server libmysqlclient-dev libpq-dev vim tmux nginx build-essential libgtk2.0-dev 
 sudo apt-get install git tig redis-server zsh subversion openjdk-7-jdk fcitx fcitx-table-all
-sudo apt-get install imagemagick libmagickcore-dev libmagickwand-dev  
+sudo apt-get install imagemagick libmagickcore-dev libmagickwand-dev nodejs  
 
 curl https://raw.githubusercontent.com/ifool/dev_conf/master/.tmux-ubuntu.conf > ~/.tmux.conf
 
